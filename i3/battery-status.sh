bf9f088ae63b392319197aaf627c167ff104286e
Emil Edholm <emil@edholm.it>
2014-09-07 18:54:35 +0200

    Changed some "icons"

--- i3/battery-status.sh
+++ i3/battery-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(acpi -b | cut -d\  -f 3 | cut -d, -f1)
 case $status in
-#    "Unknown")  echo � ;;
+    "Charging") echo ⌁ ;;
     "Discharging") echo ◯ ;;
     *)  echo ● ;;
 esac
