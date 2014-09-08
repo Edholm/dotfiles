54f96a291a7fce51438b157b55f49b3369e4d4ed
Emil Edholm <emil@edholm.it>
2014-09-09 01:30:09 +0200

    Changed some icons

--- i3/battery-bar.sh
+++ i3/battery-bar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(acpi -b | cut -d\  -f 3 | cut -d, -f1)
 case $status in
-    "Charging") echo -n "⌁ ";;
+    "Charging") echo -n "  ";;
     "Discharging") echo -n "◯ " ;;
     *)  echo -n "● ";;
 esac
